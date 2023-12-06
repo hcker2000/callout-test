39d926913d4756c381b566b5da6278e6bae5ccdb
Mike Ashcraft <mashcraft@mach2.org>
2023-12-06 11:16:42 -0500

    Count down is working. TTS next

--- lib/states/text_to_speach_state.dart
+++ lib/states/text_to_speach_state.dart
@@ -6,17 +6,27 @@ import 'one_steps_state.dart';
 
 enum TextToSpeechStateEnum { running, stopped }
 
+void _runTimerLogic(TextToSpeechState state, TextToSpeechNotifier notifier) {
+  if (state.timerState == TextToSpeechStateEnum.stopped) {
+    return;
+  }
+  notifier.decrementCounter();
+}
+
 class TextToSpeechState {
-  TextToSpeechState({required this.timerState, required this.counter});
+  TextToSpeechState(
+      {required this.timerState, required this.counter, required this.timer});
 
   final TextToSpeechStateEnum timerState;
   final num counter;
+  final Timer timer;
 
   TextToSpeechState copyWith(
-      {TextToSpeechStateEnum? timerState, num? counter}) {
+      {TextToSpeechStateEnum? timerState, num? counter, Timer? timer}) {
     return TextToSpeechState(
         timerState: timerState ?? this.timerState,
-        counter: counter ?? this.counter);
+        counter: counter ?? this.counter,
+        timer: timer ?? this.timer);
   }
 }
 
@@ -32,7 +42,10 @@ class TextToSpeechNotifier extends Notifier<TextToSpeechState> {
   TextToSpeechState build() {
     ref.onDispose(() => print('TextToSpeechState.onDispose'));
     return TextToSpeechState(
-        timerState: TextToSpeechStateEnum.stopped, counter: 0);
+        timerState: TextToSpeechStateEnum.stopped,
+        counter: 0,
+        timer: Timer.periodic(
+            Duration(seconds: 1), (timer) => _runTimerLogic(state, this)));
   }
 
   void setCounter(num value) {
@@ -41,9 +54,9 @@ class TextToSpeechNotifier extends Notifier<TextToSpeechState> {
 
   void decrementCounter() {
     var newValue = state.counter - 1;
-
-    if (newValue < 0) {
-      newValue = 0;
+    print('dec');
+    if (newValue <= 0) {
+      newValue = 30; // TODO: get from the state
     }
 
     state = state.copyWith(counter: newValue);
@@ -51,8 +64,18 @@ class TextToSpeechNotifier extends Notifier<TextToSpeechState> {
 
   void toggleState() {
     var newValue = TextToSpeechStateEnum.running;
+    // Cancel the existing timer before creating a new one
+    state.timer.cancel();
+
     if (state.timerState == TextToSpeechStateEnum.running) {
       newValue = TextToSpeechStateEnum.stopped;
+    } else {
+      state = state.copyWith(
+        timer: Timer.periodic(
+          Duration(seconds: 1),
+          (timer) => _runTimerLogic(state, this),
+        ),
+      );
     }
 
     state = state.copyWith(timerState: newValue);
